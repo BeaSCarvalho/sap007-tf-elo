94a3f518d1b371d2b3d3b178dbcd5665ba0ff46b
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2022-06-29 16:08:11 -0300

    Feat/InputSearch
    Co-Authored-By: Mônica Peixoto Guimarães <97262052+MonicaGuimaraes@users.noreply.github.com>
    Co-Authored-By: Tati-Mendonca <tatimendonz@gmail.com>
    Co-Authored-By: WayraArendartchukCastro <wayraac@gmail.com>
    Co-Authored-By: taizesantos < taize.onze@gmail.com>
    Co-Authored_By: BeaSCarvalho <beaproscarva@gmail.com>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -13,7 +13,7 @@ function Home() {
                 <img src={logo} alt="Logo Finder Github" className={styles.gif}></img>
                 <aside className={styles.aside}>
                     <Divider variant="middle" sx={{ boxShadow: "2px 2px 0.2em grey" }} />
-                    < InputSearch />
+                    < InputSearch onClick={handleSubmit} onChange={event => props.setSearchedUser(event.target.value)}/>
                     <h1> PLATAFORMA <strong className={styles.perfect}>(PER)FEITA</strong> PARA ENCONTRAR Devs DE TODOS OS LUGARES!</h1>
                     <p className={styles.textAbout}> Aqui, você pode buscar por novas parcerias ou simplesmente conhecer o trabalho de outros <strong className={styles.bold}>desenvolvedores</strong>.</p>
                     <Divider variant="middle" sx={{ boxShadow: "2px 2px 0.2em black" }} />
