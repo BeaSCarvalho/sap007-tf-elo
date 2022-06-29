c517e71f69df8307021a315e340662837495e20e
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2022-06-29 15:23:26 -0300

    Feat/Footer and Diviser
    Co-Authored-By: Mônica Peixoto Guimarães <97262052+MonicaGuimaraes@users.noreply.github.com>
    Co-Authored-By: Tati-Mendonca <tatimendonz@gmail.com>
    Co-Authored-By: WayraArendartchukCastro <wayraac@gmail.com>
    Co-Authored-By: taizesantos < taize.onze@gmail.com>
    Co-Authored_By: BeaSCarvalho <beaproscarva@gmail.com>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import logo from "../../img/logo.gif"
 import InputSearch from "../../components/input";
 import styles from "./home.module.css"
-// import Divider from '@mui/material/Divider';
-
+import Footer from "../../components/footer";
+import Divider from '@mui/material/Divider';
 
 function Home() {
 
     return (
-        <div className={styles.container}>
-            <img src={logo} alt="Logo Finder Github" className={styles.gif}></img>
-            
-            <aside className={styles.aside}>
-            < InputSearch />
-                <h1> PLATAFORMA <strong className={styles.perfect}>(PER)FEITA</strong> PARA ENCONTRAR Devs DE TODOS OS LUGARES!</h1>
-                <p className={styles.textAbout}> Aqui, você pode buscar por novas parcerias ou simplesmente conhecer o trabalho de outros <strong className={styles.bold}>desenvolvedores</strong>.</p>
-            </aside>
-        </div>
+        <>
+            <div className={styles.container}>
+                <img src={logo} alt="Logo Finder Github" className={styles.gif}></img>
+                <aside className={styles.aside}>
+                    <Divider variant="middle" sx={{ boxShadow: "2px 2px 0.2em grey" }} />
+                    < InputSearch />
+                    <h1> PLATAFORMA <strong className={styles.perfect}>(PER)FEITA</strong> PARA ENCONTRAR Devs DE TODOS OS LUGARES!</h1>
+                    <p className={styles.textAbout}> Aqui, você pode buscar por novas parcerias ou simplesmente conhecer o trabalho de outros <strong className={styles.bold}>desenvolvedores</strong>.</p>
+                    <Divider variant="middle" sx={{ boxShadow: "2px 2px 0.2em black" }} />
+                </aside>
+            </div>
+            <Footer />
+        </>
     )
 }
 
