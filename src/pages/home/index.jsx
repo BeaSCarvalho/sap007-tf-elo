1a61308f6ea44db1ae4f9482701ac39eb235ef69
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2022-06-30 02:12:06 -0300

    Feat/Favicon
    Co-Authored-By: Mônica Peixoto Guimarães <97262052+MonicaGuimaraes@users.noreply.github.com>
    Co-Authored-By: Tati-Mendonca <tatimendonz@gmail.com>
    Co-Authored-By: WayraArendartchukCastro <wayraac@gmail.com>
    Co-Authored-By: taizesantos < taize.onze@gmail.com>
    Co-Authored_By: BeaSCarvalho <beaproscarva@gmail.com>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React from "react";
 import logo from "../../img/logo.gif"
 import InputSearch from "../../components/input";
 import styles from "./home.module.css"
@@ -9,24 +9,24 @@ import { useNavigate } from "react-router-dom";
 
 
 Home.propTypes = {
-  setSearchedUser: PropTypes.func
+    setSearchedUser: PropTypes.func
 };
 
-function Home( props ) {
-  const navigate = useNavigate();
-  
-    function handleSubmit(e){
-    e.preventDefault();
-    navigate(`../results`)
-  }
-  
+function Home(props) {
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        navigate(`../results`)
+    }
+
     return (
         <>
             <div className={styles.container}>
                 <img src={logo} alt="Logo Finder Github" className={styles.gif}></img>
                 <aside className={styles.aside}>
-                    <Divider variant="middle" sx={{ boxShadow: "2px 2px 0.2em grey" }} />
-                    < InputSearch onClick={handleSubmit} onChange={event => props.setSearchedUser(event.target.value)}/>
+                    <Divider variant="middle" sx={{ marginBottom: "10%", boxShadow: "2px 2px 0.2em black" }} />
+                    <InputSearch onClick={handleSubmit} onChange={event => props.setSearchedUser(event.target.value)} />
                     <h1> PLATAFORMA <strong className={styles.perfect}>(PER)FEITA</strong> PARA ENCONTRAR Devs DE TODOS OS LUGARES!</h1>
                     <p className={styles.textAbout}> Aqui, você pode buscar por novas parcerias ou simplesmente conhecer o trabalho de outros <strong className={styles.bold}>desenvolvedores</strong>.</p>
                     <Divider variant="middle" sx={{ boxShadow: "2px 2px 0.2em black" }} />
